536181a18edd64699e9597609172149dfb52f0d1
Joao PC Windos <augusto_casotti@hotmail.com>
2022-09-11 14:16:35 -0300

    Atualizando os cards

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,46 +1,64 @@
 <template>
-  <div>
-    <h1>{{num}} {{upper(name) }}</h1>
-    <small>{{url}}</small>
+  <div id="pokemon">
+    
+    <div class="card">
+      <div class="card-image">
+        <figure>
+          <img :src="pokemons.front" alt="Placeholder image" />
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4">{{num}}-{{ upper(name) }}</p>
+        <p class="subtitle is-6">{{ pokemons.type }}</p>
+      </div>
+      <div class="card-content">
+        <div class="content">
+        
+
+          <br />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-  created:function () {
-    axios.get(this.url).then(res=>{
+  created: function () {
+    axios.get(this.url).then((res) => {
+      this.pokemons.type = res.data.types[0].type.name;
+      this.pokemons.front = res.data.sprites.front_default;
+      this.pokemons.back = res.data.sprites.back_default;
       
-      this.pokemons.type=res.data.types[0].type.name;
-      this.pokemons.front=res.data.sprites.front_default;
-      this.pokemons.back=res.data.sprites.back_default;
-      console.log(this.pokemons);
-    })
-    
+    });
   },
-  data(){
-    return{
-      pokemons:{}
-    }
+  data() {
+    return {
+      pokemons: {
+        type: "",
+        front: "",
+        back: "",
+      },
+    };
   },
-  props:{
-    num:Number,
-    name:String,
-    url:String,
-   
+  props: {
+    num: Number,
+    name: String,
+    url: String,
   },
-  methods:{
-   upper:function (value) {
-    var newName =value[0].toUpperCase() +value.slice(1);
-    return newName;
-    
-   }
-  }
-
-
-}
+  methods: {
+    upper: function (value) {
+      var newName = value[0].toUpperCase() + value.slice(1);
+      return newName;
+    },
+  },
+};
 </script>
 
 <style>
-
+ #pokemon{
+  margin-top:2%;
+ 
+}
 </style>
