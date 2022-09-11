a1b6068b332f975d49d1e4d23746a2db8b1e049e
Joao PC Windos <augusto_casotti@hotmail.com>
2022-09-11 15:00:18 -0300

    sitema de buscas

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="app">
-    <div class="column is-half is-offset-one-quarter">
 
-     <div v-for="(poke,index) in pokemons " :key="index">
+  <div class="app">
+
+    <div class="column is-half is-offset-one-quarter">
+      <img src="./assets/pokebolas.png" alt="pokebolas">
+      <input type="text" placeholder="Buscar Pokemom com o nome" class="input is-rounded" v-model="busca">
+      <button class="button is-rounded" id="buscaBTN" @click="buscar">Buscar</button>
+     
+     
+     <div v-for="(poke,index) in filterpoke " :key="poke.url">
       <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
     
 
@@ -20,19 +26,47 @@ export default{
   name:'App',
   data(){
     return{
-      pokemons:[]
+      pokemons:[],
+      busca:'',
+      filterpoke:[]
+
     }
   },
   created:function () {
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(dados=>{
       
       this.pokemons=dados.data.results;
+      this.filterpoke=dados.data.results
+
     }).catch(err=>{
       console.log(err)
     })
   },
   components:{
     Pokemon
+  },
+  computed:{
+    resultBusca:function () {
+      if(this.busca==""|| this.busca==""){
+        return this.pokemons
+     }else{
+       return this.pokemons.filter(pokemon =>pokemon.name==this.busca)
+     }
+    }
+  },methods:{
+    buscar:function () {
+      this.filterpoke=this.pokemons;
+       if(this.busca==""|| this.busca==""){
+
+       this.filterpoke=this.pokemons;
+        
+     }else{
+      this.filterpoke=this.pokemons.filter(pokemon =>pokemon.name==this.busca);
+       
+     }
+
+      
+    }
   }
   
 }
@@ -47,5 +81,8 @@ export default{
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#buscaBTN{
+  margin-top: 1%;
 }
 </style>
