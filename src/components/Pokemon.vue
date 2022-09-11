776b79f757be41fefa34cb8dbbe02230e503cb39
Joao PC Windos <augusto_casotti@hotmail.com>
2022-09-11 12:43:03 -0300

    Pegando informaçoes de API

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -6,11 +6,28 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+  created:function () {
+    axios.get(this.url).then(res=>{
+      
+      this.pokemons.type=res.data.types[0].type.name;
+      this.pokemons.front=res.data.sprites.front_default;
+      this.pokemons.back=res.data.sprites.back_default;
+      console.log(this.pokemons);
+    })
+    
+  },
+  data(){
+    return{
+      pokemons:{}
+    }
+  },
   props:{
     num:Number,
     name:String,
-    url:String
+    url:String,
+   
   },
   methods:{
    upper:function (value) {
